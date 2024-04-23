162c306d0e3b243f4bbdcfc7f5776b878dcce3ab
narayana4devops <lakshmin41@gmail.com>
2024-04-24 06:45:50 +0800

    adding backend script

--- 04-current_script.sh
+++ 04-current_script.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 COURSE="DevOps Current script"
+A=10
 
 echo "Before calling other script, course: $COURSE"
+echo "Before calling other script, A: $A"
 echo "Process ID of current shell script: $$"
 
 chmod +x 05-other_script.sh
 ./05-other_script.sh
 
 echo "After calling other script, course: $COURSE"
+echo "After calling other script, A: $A"
 #echo "Process ID of current shell script: $$"
--- 05-other_script.sh
+++ 05-other_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 COURSE="DevOps Other script"
+A=20
 
 echo "calling other script, course: $COURSE"
 echo "Process ID of other shell script: $$"
