d997aea2bce400243866250085291577348061b4
narayana4devops <lakshmin41@gmail.com>
2024-04-24 06:17:41 +0800

    adding backend script

--- 04-current_script.sh
+++ 04-current_script.sh
@@ -5,6 +5,7 @@ COURSE="DevOps Current script"
 echo "Before calling other script, course: $COURSE"
 echo "Process ID of current shell script: $$"
 
+chmod +x 05-other_script.sh
 ./05-other_script.sh
 
 echo "After calling other script, course: $COURSE"
